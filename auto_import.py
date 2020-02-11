a1d3000f6243cb8d5dddb90f126f9c4cb39b0ed8
Nicolas Welsh <nicolasawelsh@gmail.com>
2020-02-11 10:47:09 -0500

    smart descriptions

--- auto_import.py
+++ auto_import.py
@@ -15,53 +15,116 @@ def main():
         raw_li.append(i.splitlines())
     
     li = []
+    li = handle_filtering(li, raw_li)
+    
+
+    for elem in li:
+        print(elem)
+
+def handle_filtering(li, raw_li):
     for item in raw_li:
         if len(item) == 7:
-            handle_inconsistency(item)
-        
+            if 'REPORTED' in item[3]:
+                # Remove HTML tags
+                if '<p>' in item: item.remove('<p>')
+                if '</p>' in item: item.remove('</p>')
+                while ' <br/>' in item:
+                    item.remove(' <br/>')
 
-def handle_inconsistency(li):
-    # Likely incomprehenisble if doesn't include 'REPORTED'
-    if 'REPORTED' in li[3]:
-        # Remove HTML tags
-        if '<p>' in li: li.remove('<p>')
-        if '</p>' in li: li.remove('</p>')
-        while ' <br/>' in li:
-            li.remove(' <br/>')
+                # Handle inconsistencies in ind: 0
+                if 'CAD' in item[0]:
+                    item[0] = item[0].replace("CAD", "")
+                    item[0] = item[0].replace("#", "")
+                    item[0] = item[0].replace(" ", "")
+                    item[0] = item[0].replace("-", "")
+                if 'CSA' in item[0]:
+                    item[0] = -1
 
-        # Handle inconsistencies in ind: 0
-        if 'CAD' in li[0]:
-            li[0] = li[0].replace("CAD", "")
-            li[0] = li[0].replace("#", "")
-            li[0] = li[0].replace(" ", "")
-            li[0] = li[0].replace("-", "")
-        if 'CSA' in li[0]:
-            li[0] = -1
+                # Convert CAD# str to int
+                item[0] = int(item[0])
+                desc = item[-1]
+                item.pop(-1)
 
-        # Convert CAD# str to int
-        li[0] = int(li[0])
+                # Handle inconsistencies in ind: 1
+                item[1] = item[1].replace("REPORTED", "")
+                item[1] = item[1].replace(" ", "")
+                if 'DATE' in item[1]:
+                    item[1] = item[1].replace("DATE", "")
 
-        # Temporarily remove description
-        descr = li[-1]
-        li.pop(-1)
+                x = item[1].split(",")
+                item.pop(1)
+                for i in x:
+                    item.append(i)
 
-        # Handle inconsistencies in ind: 1
-        li[1] = li[1].replace("REPORTED", "")
-        li[1] = li[1].replace(" ", "")
-        if 'DATE' in li[1]:
-            li[1] = li[1].replace("DATE", "")
 
-        x = li[1].split(",")
-        li.pop(1)
-        for i in x:
-            li.append(i)
+                smart_desc = handle_description(desc)
+                for i in smart_desc:
+                    item.append(i)
 
-        if len(li) == 3:
-            li.append(descr) # Add description back on
-            li.append(li)
-            print(li)
+                if len(item) == 5:
+                    li.append(item)
+                    
+    return li
 
-#def handle_description(li):
+def handle_description(desc):
+    smart_desc = []
+
+    if 'THEFT' in desc:
+        crime = 'THEFT'
+    elif 'TREPASSING' in desc:
+        crime = 'TREPASSING'
+    elif 'LIQUOR LAW VIOLATIONS' in desc:
+        crime = 'LIQUOR LAW VIOLATIONS'
+    elif 'DRUG ABUSE VIOLATIONS' in desc:
+        crime = 'DRUG ABUSE VIOLATIONS'
+    elif 'FRAUD' in desc:
+        crime = 'FRAUD'
+    elif 'POSSESSION' in desc:
+        crime = 'POSSESSION'
+    elif 'VANDALISM' in desc:
+        crime = 'VANDALISM'
+    elif 'PUBLIC INTOXICATION' in desc:
+        crime = 'PUBLIC INTOXICATION'
+    elif 'RECKLESS DRIVER' in desc:
+        crime = 'RECKLESS DRIVER'
+    elif 'ASSAULT' in desc:
+        crime = 'ASSAULT'
+    elif 'BATTERY' in desc:
+        crime = 'BATTERY'
+    elif 'SEX OFFENSE' in desc:
+        crime = 'SEX OFFENSE'
+    elif 'BURGLARY' in desc:
+        crime = 'BURGLARY'
+    else:
+        crime = 'OTHER'
+
+    smart_desc.append(crime)
+
+    if 'UNDER INVESTIGATION' in desc:
+        status = 'UNDER INVESTIGATION'
+    elif 'ARREST MADE' in desc:
+        status = 'ARREST MADE'
+    elif 'NOTHING FURTHER' in desc:
+        status = 'NOTHING FURTHER'
+    elif 'UNABLE TO LOCATE' in desc:
+        status = 'UNABLE TO LOCATE'
+    elif 'CASE CLOSED' in desc:
+        status = 'CASE CLOSED'
+    elif 'UNFOUNDED' in desc:
+        status = 'UNFOUNDED'
+    elif 'INACTIVE' in desc:
+        status = 'INACTIVE'
+    elif 'CLOSED' in desc:
+        status = 'CLOSED'
+    elif 'PENDING PROSECUTOR REVIEW' in desc:
+        status = 'PENDING PROSECUTOR REVIEW'
+    else:
+        status = 'OTHER'
+    
+    smart_desc.append(status)
+
+    return smart_desc
+
 
 if __name__ == '__main__':
     main()
